62092b021b3efc3531a609162f79421d8a8c91a5
Tommy Christenson <tchristenson21@gmail.com>
2023-03-17 13:35:05 -0500

    Added firstName, lastName to Users table and modified Seeder for firstName, lastName

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,6 +9,41 @@ const { restoreUser } = require("../../utils/auth.js");
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
+// // GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+// // GET /api/restore-user
+// router.use(restoreUser);
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
