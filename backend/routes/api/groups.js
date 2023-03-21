f707f56ed3f0dc75dee538c9d1299a422ed7a481
Tommy Christenson <tchristenson21@gmail.com>
2023-03-21 16:03:16 -0500

    create a group end point

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { Group, Membership, GroupImage } = require('../../db/models');
 
 const { check } = require('express-validator');
@@ -11,6 +11,18 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+// Get all groups
 router.get('/', async (req, res, next) => {
   const groups = await Group.findAll()
   let groupsArr = [];
@@ -24,25 +36,72 @@ router.get('/', async (req, res, next) => {
         groupId: currGroup.id
       }
     })
-    // console.log(currGroup)
-    // console.log(count)
     currGroup.numMembers = count;
 
-    console.log(currGroup)
-    const groupImages = await GroupImage.findAll()
-
-    for (let i = 0; i < groupImages.length; i++) {
-      let currGroupImage = groupImages[i].toJSON()
-      // console.log(currGroupImage)
-      // console.log(typeof currGroupImage)
-      if (currGroupImage.id === currGroup.id) {
-        currGroup.previewImage = currGroupImage.url
+    // console.log(currGroup)
+    const groupImages = await GroupImage.findAll({
+      where: {
+        groupId: currGroup.id
       }
-    }
-      groupsArr.push(currGroup)
+    })
+
+    let groupImage = groupImages[0].toJSON()
+    currGroup.previewImage = groupImage.url
+
+    groupsArr.push(currGroup)
   }
+
   finalObj.Groups = groupsArr
   res.json(finalObj)
+})
+
+// Get all groups joined or organized by the Current User
+router.get('/current', requireAuth, async (req, res, next) => {
+  const userId = req.user.id
+})
+
+// Create a group
+router.post('/', requireAuth, async (req, res, next) => {
+  try {
+    const { name, about, type, private, city, state } = req.body
+    const organizerId = req.user.id
+    // console.log(req.user);
+    // console.log(organizerId)
+    // console.log(name)
+
+    if (req.user) {
+      console.log('is this hitting')
+      const newGroup = await Group.create({
+        organizerId: organizerId,
+        name,
+        about,
+        type,
+        private,
+        city,
+        state
+      })
+      console.log(newGroup)
+
+      res.status(201).json(newGroup);
+    }
+
+  } catch(err) {
+    let newErr = new Error()
+    newErr.message = 'Bad Request'
+
+    newErr.errors = {};
+
+    newErr.status = 400;
+
+    newErr.errors.name = 'Name must be 60 characters or less'
+    newErr.errors.about = 'About must be 50 chracters or more'
+    newErr.errors.type = "Type must be 'Online' or 'In person'"
+    newErr.errors.private = 'Private must be a boolean'
+    newErr.errors.city = 'City is required'
+    newErr.errors.state = 'State is required'
+
+    next(newErr);
+  }
 })
 
 
