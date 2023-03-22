580726a1ce378c2e2a89d374d4c5b41ee4fa003d
Tommy Christenson <tchristenson21@gmail.com>
2023-03-21 19:42:28 -0500

    Get all groups joined or organized by the current user endpoint

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -67,6 +67,115 @@ router.get('/', async (req, res, next) => {
   res.json(finalObj)
 })
 
+
+// GET ALL GROUPS JOINED OR ORGANIZED BY THE CURRENT USER
+router.get('/current', requireAuth, async (req, res, next) => {
+  const userId = req.user.id
+  const organizedGroups = await Group.findAll({
+    where: {
+      [Op.or]: [
+        {organizerId: userId},
+        {'$memberships.userId$': userId}
+      ]
+    },
+    include: [
+      {
+        model: Membership,
+        attributes: [],
+        include: [
+          {
+            model: User,
+            attributes: []
+          }
+        ]
+      }
+    ]
+  })
+
+  let groupsArr = [];
+  let finalObj = {};
+
+  for (let i = 0; i < organizedGroups.length; i++) {
+    let currGroup = organizedGroups[i].toJSON()
+
+    let count = await Membership.count({
+      where: {
+        groupId: currGroup.id
+      }
+    })
+    currGroup.numMembers = count;
+
+    // console.log(currGroup)
+    const groupImages = await GroupImage.findAll({
+      where: {
+        groupId: currGroup.id,
+        preview: true
+      }
+    })
+    // console.log(groupImages)
+    if (groupImages.length) {
+      let groupImage = groupImages[0].toJSON()
+      // console.log(groupImage)
+
+        currGroup.previewImage = groupImage.url
+
+    } else {
+      currGroup.previewImage = 'no preview image available'
+    }
+
+    groupsArr.push(currGroup)
+  }
+
+  finalObj.Groups = groupsArr
+  res.json(finalObj)
+
+})
+
+
+// CREATE A GROUP
+router.post('/', requireAuth, async (req, res, next) => {
+  try {
+    const { name, about, type, private, city, state } = req.body
+    const organizerId = req.user.id
+    // console.log(req.user);
+    // console.log(organizerId)
+    // console.log(name)
+
+    if (req.user) {
+      console.log('is this hitting')
+      const newGroup = await Group.create({
+        organizerId: organizerId,
+        name,
+        about,
+        type,
+        private,
+        city,
+        state
+      })
+      console.log(newGroup)
+
+      res.status(201).json(newGroup);
+    }
+
+  } catch(err) {
+    let newErr = new Error()
+    newErr.message = 'Bad Request'
+
+    newErr.errors = {};
+
+    newErr.status = 400;
+
+    newErr.errors.name = 'Name must be 60 characters or less'
+    newErr.errors.about = 'About must be 50 chracters or more'
+    newErr.errors.type = "Type must be 'Online' or 'In person'"
+    newErr.errors.private = 'Private must be a boolean'
+    newErr.errors.city = 'City is required'
+    newErr.errors.state = 'State is required'
+
+    next(newErr);
+  }
+})
+
 // GET DETAILS OF A GROUP FROM AN ID
 router.get('/:groupId', async (req, res, next) => {
 
@@ -114,62 +223,6 @@ router.get('/:groupId', async (req, res, next) => {
 
     next(newErr);
 
-  }
-
-})
-
-
-
-
-// GET ALL GROUPS JOINED OR ORGANIZED BY THE CURRENT USER
-router.get('/current', requireAuth, async (req, res, next) => {
-  const userId = req.user.id
-
-})
-
-
-
-// CREATE A GROUP
-router.post('/', requireAuth, async (req, res, next) => {
-  try {
-    const { name, about, type, private, city, state } = req.body
-    const organizerId = req.user.id
-    // console.log(req.user);
-    // console.log(organizerId)
-    // console.log(name)
-
-    if (req.user) {
-      console.log('is this hitting')
-      const newGroup = await Group.create({
-        organizerId: organizerId,
-        name,
-        about,
-        type,
-        private,
-        city,
-        state
-      })
-      console.log(newGroup)
-
-      res.status(201).json(newGroup);
-    }
-
-  } catch(err) {
-    let newErr = new Error()
-    newErr.message = 'Bad Request'
-
-    newErr.errors = {};
-
-    newErr.status = 400;
-
-    newErr.errors.name = 'Name must be 60 characters or less'
-    newErr.errors.about = 'About must be 50 chracters or more'
-    newErr.errors.type = "Type must be 'Online' or 'In person'"
-    newErr.errors.private = 'Private must be a boolean'
-    newErr.errors.city = 'City is required'
-    newErr.errors.state = 'State is required'
-
-    next(newErr);
   }
 })
 
