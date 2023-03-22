81169ddc8fec38a8d313a1f640422a46522ebd10
Tommy Christenson <tchristenson21@gmail.com>
2023-03-22 14:08:19 -0500

    edit a Venue by ID in process

---
+++ backend/routes/api/venues.js
@@ -0,0 +1,55 @@
+// backend/routes/api/venues.js
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User, Group, Membership, GroupImage, Venue } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const group = require('../../db/models/group');
+const { VERSION_NUMBER } = require('sqlite3');
+
+const router = express.Router();
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+// EDIT A VENUE SPECIFIED BY ITS ID
+router.put('/:venueId', validateLogin, async (req, res, next) => {
+  let editedVenue = await Venue.findOne({
+    where: {
+      [Op.or]: [
+        {'$groups.organizerId$': req.user.id},
+        {'$memberships.status$': 'co-host'}
+      ]
+    },
+    include: [
+      {
+        model: Group,
+        attributes: [],
+        include: {
+          model: Membership,
+          attributes: []
+        }
+      }
+    ]
+  })
+  console.log(editedVenue)
+  res.json(editedVenue)
+
+})
+
+
+
+
+module.exports = router;
