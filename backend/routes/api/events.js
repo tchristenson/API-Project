7ef7db80a16a6630682ad542141154b543bf517b
Tommy Christenson <tchristenson21@gmail.com>
2023-03-22 22:11:02 -0500

    get all Events - still need to add Num Attending

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -4,7 +4,7 @@ const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { Event, User, Group, Membership, GroupImage, Venue } = require('../../db/models');
+const { EventImage, Event, User, Group, Membership, GroupImage, Venue } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -26,7 +26,49 @@ const validateLogin = [
 
 // GET ALL EVENTS
 router.get('/', async (req, res, nexr) => {
+  let eventArr = [];
+  let finalObj = {};
 
+  let events = await Event.findAll({
+    attributes: {
+      exclude: ['description', 'capacity', 'price', 'createdAt', 'updatedAt']
+    },
+    include: [
+      {
+        model: Group,
+        attributes: ['id', 'name', 'city', 'state']
+      },
+      {
+        model: Venue,
+        attributes: ['id', 'city', 'state']
+      }
+    ]
+  })
+
+  for (let i = 0; i < events.length; i++) {
+    currEvent = events[i]
+    currEvent = currEvent.toJSON()
+
+    const eventImages = await EventImage.findAll({
+      where: {
+        eventId: currEvent.id,
+        preview: true
+      }
+    })
+
+    if (eventImages.length) {
+      let eventImage = eventImages[0].toJSON()
+      currEvent.previewImage = eventImage.url
+
+    } else {
+      currEvent.previewImage = 'no preview image available'
+    }
+
+    eventArr.push(currEvent)
+  }
+
+  finalObj.Events = eventArr
+  res.status(200).json(finalObj)
 })
 
 
