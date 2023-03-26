a6db154dbe58b585373447817215402236e06eff
Tommy Christenson <tchristenson21@gmail.com>
2023-03-26 00:40:09 -0500

    Made venue address unique

--- backend/db/migrations/20230320122640-create-venue.js
+++ backend/db/migrations/20230320122640-create-venue.js
@@ -26,7 +26,8 @@ module.exports = {
       },
       address: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true
       },
       city: {
         type: Sequelize.STRING,
