67aa3026acb74d0eedcc0fa785ad232cb799ef44
Tommy Christenson <tchristenson21@gmail.com>
2023-04-10 14:54:18 -0500

    starting new group form

--- frontend/src/components/GroupNew/index.js
+++ frontend/src/components/GroupNew/index.js
@@ -1,10 +1,25 @@
+import {useState, useEffect} from 'react'
+import { useHistory } from 'react-router-dom';
 import './GroupNew.css'
 
 function NewGroup() {
+
+
+
+
+
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+  }
+
   return(
-    <div>
-      Hello from New Group Componeent
-    </div>
+    <form className='new-group-form' onSubmit={handleSubmit}>
+      <h3>BECOME AN ORGANIZER</h3>
+      <h2>We'll walk you through a few steps to build your local community</h2>
+    </form>
   )
 }
 
