4962f4a85b62096e2d432538c3297e921a09f4dd
Tommy Christenson <tchristenson21@gmail.com>
2023-04-12 17:27:28 -0500

    create event form - in process

--- frontend/src/components/EventForm/index.js
+++ frontend/src/components/EventForm/index.js
@@ -6,14 +6,9 @@ import './EventNew.css'
 
 function EventForm({event, formType}) {
   const dispatch = useDispatch()
-  const [errors, setErrors] = useState({})
-  const [hasSubmitted, setHasSubmitted] = useState(false)
   const {groupId} = useParams()
-  const group = useSelector(state => state.groups[groupId])
-  console.log('group inside of Event Form', group)
-
-
   const history = useHistory();
+
   const [name, setName] = useState(event?.name)
   const [type, setType] = useState(event?.type)
   const [isPrivate, setIsPrivate] = useState(event?.isPrivate)
@@ -22,6 +17,8 @@ function EventForm({event, formType}) {
   const [endDate, setEndDate] = useState(event?.endDate)
   const [url, setUrl] = useState(event?.url)
   const [description, setDescription] = useState(event?.description)
+  const [errors, setErrors] = useState({})
+  const [hasSubmitted, setHasSubmitted] = useState(false)
 
   useEffect(() => {
     if (hasSubmitted) {
@@ -37,6 +34,10 @@ function EventForm({event, formType}) {
       setErrors(newErrors)
     }
   }, [name, type, isPrivate, price, startDate, endDate, url, description])
+
+  const group = useSelector(state => state.groups[groupId])
+  if (!group) return null
+  console.log('group inside of Event Form', group)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -80,6 +81,87 @@ function EventForm({event, formType}) {
   return (
     <form className="new-event-form" onSubmit={handleSubmit}>
       <h2>{`Create an event for ${group.name}`}</h2>
+
+      <div className="event-name-wrapper">
+        <h4>What is the name of your event?</h4>
+        <input
+            type="text"
+            placeholder="Event Name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          {errors.name && (<p className='errors'>{errors.name}</p>)}
+      </div>
+
+      <div className="type-private-price-wrapper">
+        <h4>Is this an in person or online event?</h4>
+        <select value={type} onChange={e => setType(e.target.value)}>
+          <option value="">{'(select one)'}</option>
+          <option value={'Online'}>Online</option>
+          <option value={'In Person'}>In Person</option>
+        </select>
+        {errors.type && (<p className='errors'>{errors.type}</p>)}
+
+        <h4>Is this event private or public?</h4>
+        <select value={isPrivate} onChange={e => setType(e.target.value)}>
+          <option value="">{'(select one)'}</option>
+          <option value={true}>Private</option>
+          <option value={false}>Public</option>
+        </select>
+        {errors.isPrivate && (<p className='errors'>{errors.isPrivate}</p>)}
+
+        <h4>What is the price of your event?</h4>
+        <input
+          type="number"
+          placeholder="0"
+          value={price}
+          onChange={e => setPrice(e.target.value)}
+        />
+        {errors.price && (<p className='errors'>{errors.price}</p>)}
+      </div>
+
+      <div className="date-wrapper">
+        <h4>When does your event start?</h4>
+        <input
+          type="datetime-local"
+          value={startDate}
+          onChange={e => setPrice(e.target.value)}
+        />
+        {errors.startDate && (<p className='errors'>{errors.startDate}</p>)}
+
+        <h4>When does your event end?</h4>
+        <input
+          type="datetime-local"
+          value={endDate}
+          onChange={e => setPrice(e.target.value)}
+        />
+        {errors.endDate && (<p className='errors'>{errors.endDate}</p>)}
+      </div>
+
+      <div className="url-wrapper">
+        <h4>Please add an image url for your event below</h4>
+        <input
+            type="text"
+            placeholder="Image URL"
+            value={url}
+            onChange={e => setUrl(e.target.value)}
+          />
+          {errors.url && (<p className='errors'>{errors.url}</p>)}
+      </div>
+
+      <div>
+        <h4>Please describe your event</h4>
+        <textarea
+            placeholder="Please include at least 30 characters"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          {errors.description && (<p className='errors'>{errors.description}</p>)}
+      </div>
+
+      <div className='submit-button'>
+        <button type="submit">{formType === 'Create Event' ? 'Create Event' : 'Update Event' }</button>
+      </div>
 
     </form>
   )
