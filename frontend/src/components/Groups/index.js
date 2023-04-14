c93a27b77713472176e8f1268cd43e23e28064c2
Tommy Christenson <tchristenson21@gmail.com>
2023-04-14 01:04:56 -0500

    Changing how I get All Groups - dispatching thunk instead of useSelector

--- frontend/src/components/Groups/index.js
+++ frontend/src/components/Groups/index.js
@@ -2,20 +2,48 @@ import { NavLink } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react"
 import { getAllGroupsThunk } from "../../store/groups"
+import { useState } from "react"
 import "./Groups.css"
 
 function Groups() {
   const dispatch = useDispatch()
   // console.log('groups inside of Groups component', groups)
 
+  // // original
+  // useEffect(() => {
+  //   dispatch(getAllGroupsThunk())
+  // }, [dispatch])
+
+  // const groups = useSelector(state => state.groups)
+
+  // // original stop
+
+  // attempt
+  const [groups, setGroups] = useState('')
+
   useEffect(() => {
     dispatch(getAllGroupsThunk())
+    .then((data) => setGroups(data))
   }, [dispatch])
 
-  const groups = useSelector(state => state.groups)
+  console.log('groups inside All Groups', groups)
+  console.log('groups.Groups inside All Groups', groups.Groups)
+
+  if (!groups) return null
+
+
+
+
+
+
+
+
+
+// Change const groupList to equal groupsArr.map if going back to original
+// also now returning from the getAllGroupsThunk whereas I wasn't prior
   const groupsArr = Object.values(groups)
 
-  const groupList = groupsArr.map(group => (
+  const groupList = groups.Groups.map(group => (
     <NavLink className="nav-link" to={`/groups/${group.id}`}>
       <div className="single-group" key={group.id}>
         <div className="group-image">
