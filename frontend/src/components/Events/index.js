34854162566f9e97420a145af561d2a436d19c8c
Tommy Christenson <tchristenson21@gmail.com>
2023-04-16 04:17:13 -0500

    Sorted All Events page

--- frontend/src/components/Events/index.js
+++ frontend/src/components/Events/index.js
@@ -33,32 +33,29 @@ import "./Events.css"
 function Events() {
   const dispatch = useDispatch()
 
-  // // ORIGINAL METHOD
   const events = useSelector(state => state.events)
   const eventsArr = Object.values(events)
 
   useEffect(() => {
     dispatch(getAllEventsThunk())
   }, [dispatch])
-  // // ORIGINAL END
 
-
-    // // NEW APPROACH - now using thunk instead of useSelector
-    // const [events, setEvents] = useState('')
-
-    // useEffect(() => {
-    //   dispatch(getAllEventsThunk())
-    //   .then((data) => setEvents(data))
-    // }, [dispatch])
-
-    console.log('events inside All Events', events)
+    // console.log('events inside All Events', events)
+    // console.log('eventsArr inside of All Events', eventsArr)
     // console.log('events.Events inside All Events', events.Events)
 
     if (!events) return null
 
+    // const sortedEventsArr = eventsArr.sort((event1, event2) => new Date(event1.startDate).getTime() - new Date(event2.startDate).getTime())
+    // console.log(sortedEventsArr)
+
+    const currentDate = new Date()
 
 // I was mapping over eventsArr in the original approach
-  const eventList = eventsArr.map(event => (
+  const eventList = eventsArr.filter(event =>
+    new Date(event.startDate).getTime() > currentDate.getTime())
+    .sort((event1, event2) => new Date(event1.startDate).getTime() - new Date(event2.startDate).getTime())
+    .map(event => (
     <NavLink className="nav-link" to={`/events/${event.id}`}>
       <div className="single-event" key={event.id}>
         <div className="image-event-wrapper">
@@ -80,6 +77,35 @@ function Events() {
     </NavLink>
   ))
 
+  // console.log('eventList', eventList)
+
+  const pastEventList = eventsArr.filter(event =>
+    new Date(event.startDate).getTime() < currentDate.getTime())
+    .sort((event1, event2) => new Date(event2.startDate).getTime() - new Date(event1.startDate).getTime())
+    .map(event => (
+    <NavLink className="nav-link" to={`/events/${event.id}`}>
+      <div className="single-event" key={event.id}>
+        <div className="image-event-wrapper">
+          <div className="event-image">
+           <img src={event.previewImage} alt="Group Image" />
+          </div>
+          <div className="event-info">
+            <div className="date-time-wrapper">
+              <div className="event-date">{dateTimeFix(event.startDate)[0]}</div>
+              <div><span className="dot-in-event-details-of-group-page">.</span></div>
+              <div className="event-time">{dateTimeFix(event.startDate)[1]}</div>
+            </div>
+            <div className="event-name"><h3>{event.name}</h3></div>
+            <div className="event-city-state">{event.Group.city}, {event.Group.state}</div>
+          </div>
+        </div>
+        <div className="event-about">{event.description}</div>
+      </div>
+    </NavLink>
+  ))
+
+  // console.log('pastEventList', pastEventList)
+
   return (
     <div className="content">
       <div className="event-and-group-links">
@@ -90,7 +116,10 @@ function Events() {
         <h5>Events in Meetup</h5>
       </div>
       <div className="full-event-list">
-        {eventList}
+        <>
+          {eventList}
+          {pastEventList}
+        </>
       </div>
     </div>
   )
